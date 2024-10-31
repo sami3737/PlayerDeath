b46aa02618923352f2d262eb6027ca2730778814
sami3737 <46924423+sami3737@users.noreply.github.com>
2024-10-31 17:40:27 +0100

    Add files via upload

---
+++ PlayerDeath.cs
@@ -0,0 +1,19 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("PlayerDeath", "sami37", "1.0.0")]
+    public class PlayerDeath : RustPlugin
+    {
+        private void OnEntityDeath(BaseCombatEntity entity, HitInfo info)
+        {
+            BasePlayer player = entity?.ToPlayer();
+            if (player != null)
+                DestroyEntity(player);
+        }
+
+        private void DestroyEntity(BasePlayer player)
+        {
+            if(player.net.connection.authLevel == 0)
+                ConVar.Entity.DeleteBy(player.userID);
+        }
+    }
+}
